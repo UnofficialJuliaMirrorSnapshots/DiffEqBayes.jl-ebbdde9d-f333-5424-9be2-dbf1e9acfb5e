e7ff5287f265814c3f99ff8e3a506e1d73e6627b
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 20:26:17 +0000

    Snapshot of branch master taken on 2019-04-07T16:26:16.731-04:00 from "https://github.com/UnofficialJuliaMirror/DiffEqBayes.jl-ebbdde9d-f333-5424-9be2-dbf1e9acfb5e"

---
+++ src/DiffEqBayes.jl
@@ -0,0 +1,20 @@
+module DiffEqBayes
+using DiffEqBase, Stan, Distributions, Turing, MacroTools, Mamba
+using OrdinaryDiffEq, ParameterizedFunctions, RecursiveArrayTools
+using DynamicHMC, TransformVariables, LogDensityProblems
+using Parameters, Distributions, Optim
+using Distances, ApproxBayes, StatsPlots
+
+STANDARD_PROB_GENERATOR(prob,p) = remake(prob;u0=eltype(p).(prob.u0),p=p)
+STANDARD_PROB_GENERATOR(prob::MonteCarloProblem,p) = MonteCarloProblem(remake(prob.prob;u0=eltype(p).(prob.prob.u0),p=p))
+
+include("stan_inference.jl")
+include("turing_inference.jl")
+include("stan_string.jl")
+include("utils.jl")
+include("dynamichmc_inference.jl")
+include("abc_inference.jl")
+
+export StanModel, stan_inference, turing_inference, stan_string, StanODEData, plot_chain, dynamichmc_inference, LotkaVolterraPosterior, abc_inference
+
+end # module
